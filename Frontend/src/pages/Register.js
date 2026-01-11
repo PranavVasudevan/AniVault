b5729cf36a2f8cd99a343217335fe5e53d6bd740
PranavVasudevan <pranavv211@gmail.com>
2026-01-11 22:19:22 +0530

    Changing to vite from CRA

--- Frontend/src/pages/Register.js
+++ Frontend/src/pages/Register.js
@@ -3,7 +3,8 @@ import { Link, useNavigate } from "react-router-dom";
 import "../styles/styles.css";
 
 
-const API_BASE = process.env.REACT_APP_API_URL;
+const API_BASE = import.meta.env.VITE_API_URL
+
 
 
 export default function Register() {
